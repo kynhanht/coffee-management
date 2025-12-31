9626a6d26e6d6e0346edf72b99fe3a01d604c869
kynhanht <kynhanht@gmail.com>
2025-12-31 20:38:59 +0700

    init base project

---
+++ src/main/java/com/example/coffeemanagement/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.coffeemanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("")
+    public String home(){
+        return "index";
+    }
+
+}

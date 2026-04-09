baa033a1d31c1448b02f8188a842a056b680c436
kynhanht <kynhanht@gmail.com>
2026-04-10 02:00:13 +0700

    Update Pos module

---
+++ src/main/java/com/example/coffeemanagement/service/ITableService.java
@@ -0,0 +1,24 @@
+package com.example.coffeemanagement.service;
+
+import com.example.coffeemanagement.dto.TableDTO;
+import com.example.coffeemanagement.dto.TableInfoDTO;
+import com.example.coffeemanagement.dto.TableOptionDTO;
+import com.example.coffeemanagement.dto.request.*;
+import com.example.coffeemanagement.enums.TableStatus;
+
+import java.util.List;
+
+public interface ITableService {
+
+    TableDTO getById(String id);
+    List<TableDTO> getAll();
+    TableInfoDTO getTableInfo(String id);
+    void reserveTable(ReserveTableRequest request);
+    void moveTable(MoveTableRequest request);
+    List<TableOptionDTO> getSelectableTables(String id, List<TableStatus> statues);
+    void mergeTables(MergeTableRequest request);
+    void splitTable(SplitTableRequest request);
+    void cancelTable(String id);
+
+
+}
